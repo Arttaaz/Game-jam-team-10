0316f1c9ab7fe3529473a9620b9d9ca286e87323
Gathzen <nat-97@hotmail.fr>
2018-02-05 14:13:48 +0100

    sfdklfdsj

---
+++ Window.rb
@@ -0,0 +1,16 @@
+require 'gosu'
+
+class Game < Gosu::Window
+
+  def initialize
+    super(800,800)
+    self.caption = "Rogue-like"
+  end
+
+  def update
+  end
+
+  def draw
+  end
+
+end
--- main.rb
+++ main.rb
@@ -1,18 +1,4 @@
 require 'gosu'
+load 'Window.rb'
 
-
-class Game < Gosu::Window
-
-  def initialize
-    super(800,800)
-    self.caption = "Rogue-like"
-  end
-
-  def update
-  end
-
-  def draw
-  end
-
-end
 Game.new.show()
