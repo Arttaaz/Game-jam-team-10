cdf7ed34c3a8fa318b3f776b08ea1b1d5ee95707
Gathzen <nat-97@hotmail.fr>
2018-02-06 17:52:27 +0100

    Update Skill.rb
    ffdds

--- Skill.rb
+++ Skill.rb
@@ -8,12 +8,13 @@ class Skill
 
   attr_reader :name
 
-  def initialize(name, type, target, modifier, duration = 0)
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
     @name = name
     @type = type
     @target = target
     @modifier = modifier
     @duration = duration
+    @temp = temp
   end
 
   def activate
@@ -23,43 +24,199 @@ class Skill
     if @duration != 0
       self.activate
       @duration = @duration - 1
-    elsif @temp == true
-      @modifier = -@modifier
-      self.activate
     end
   end
 
 end
 
-class LifeModifier < Skill
-
+class HealthModif < Skill
   def initialize(name, type, target, modifier, duration = 0)
     super(name, type, target, modifier, duration)
   end
 
   def activate
-    if @target.health+@modifier < 0
+    healthCheck = @target.health + (@target.health * @modifier)/100
+    if healthCheck > @target.maxHealth
+      @target.health = @target.maxHealth
+    elsif healthCheck < 0
       @target.health = 0
     else
-      @target.health = @target.health + @modifier if ((@modifier+@target.health) < (@target.maxHealth)) && (@modifier+@target.health >= 0)
-    end
-    if @type == Type::PASSIVE
-      @target.maxHealth = @target.maxHealth + @modifier
+      @target.health = healthCheck
     end
   end
-
 end
 
-class PowerModifier < Skill
+class MaxHealthModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    maxHealthCheck = @target.maxHealth + (@target.maxHealth * @modifier)/100
+    if maxHealthCheck < 0
+      @target.maxHealth = 0
+    else
+      @target.maxHealth = maxHealthCheck
+    end
+  end
+end
 
+class PowerModif< Skill
   def initialize(name, type, target, modifier, duration = 0)
     super(name, type, target, modifier, duration)
   end
 
   def activate
-    @target.power = @target.power + @modifier
-    if @type == Type::PASSIVE
-      @target.maxPower = @targer.maxPower + @modifier
+    powerCheck = @target.power + (@target.power * @modifier)/100
+    if powerCheck > @target.maxPower
+      @target.power = @target.maxPower
+    elsif powerCheck < 0
+      @target.power = 0
+    else
+      @target.power = powerCheck
+    end
+  end
+end
+
+class MaxPowerModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activite
+    maxPowerCheck = @target.maxPower + (@target.maxPower * @modifier)/100
+    if maxPowerCheck < 0
+      @target.maxPower = 0
+    else
+      @target.maxPower = maxPowerCheck
+    end
+  end
+end
+
+class DmgModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    @target.damage = @target.damage + (@target.damage * @modifier)/100
+  end
+end
+
+class DmgReducModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    dmgReducCheck = @target.dmgReduc + (@target.dmgReduc * @modifier)/100
+    if dmgReduc > 100
+      @target.dmgReduc = 100
+    else
+      @target.dmgReduc = dmgReducCheck
+    end
+  end
+end
+
+class PowerRegenModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    @target.powRegen = @target.powRegen + (@target.powerRegen * @modifier)/100
+  end
+end
+
+class ResModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    @target.phy_def = @target.phy_def + (@target.phy_def * @modifier)/100
+    @target.eng_def = @target.eng_def + (@target.eng_def * @modifier)/100
+  end
+end
+
+class PhysDefModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    @target.phy_def = @target.phy_def + (@target.phy_def * @modifier)/100
+  end
+end
+
+class EngDefModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    @target.eng_def = @target.eng_def + (@target.eng_def * @modifier)/100
+  end
+end
+
+class ExpModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    @target.expBonus = @target.expBonus + (@target.expBonus * @modifier)/100
+  end
+end
+
+class MoneyModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    @target.moneyBonus = @target.moneyBonus + (@target.moneyBonus * @modifier)/100
+  end
+end
+
+class SpeedModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    @target.speed = @target.speed + @modifier
+  end
+end
+
+class ShieldModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activate
+    shieldCheck = @target.shield + (@target.shield * @modifier)/100
+    if shieldCheck > @target.maxShield
+      @target.shield = @target.maxShield
+    elsif shield < 0
+      @target.shield = 0
+    else
+      @target.shield = shieldCheck
+    end
+  end
+end
+
+class MaxShieldModif < Skill
+  def initialize(name, type, target, modifier, duration = 0, temp = false)
+    super(name, type, target, modifier, duration, temp)
+  end
+  
+  def activiate
+    maxShieldCheck = @target.maxShield + (@target.maxShield * @modifier)/100
+    if maxShieldCheck < 0
+      @target.maxShield = 0
+    else
+      @target.maxShield = maxShieldCheck
     end
   end
 end
