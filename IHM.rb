ff7d4041325c95709a09ac03864ad7594d286ff9
AoRailgun <leiwulong38@hotmail.fr>
2018-02-06 21:10:29 +0100

    o

--- IHM.rb
+++ IHM.rb
@@ -18,7 +18,34 @@ class IHM < Gosu::Window
 
   def draw
     self.box
-    self.tabContent(2) # paramètre (tabName) ?
+    if @carte.isInside?(x, y, xx, yy)
+      draw_rect(@x+465,@y+455,610,290,Gosu::Color::BLACK, z=0, :default) #carte
+
+      draw_rect(@x+90,@y+535,60,60,Gosu::Color::WHITE, z=0, :default) #skills
+      draw_rect(@x+165,@y+535,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+240,@y+535,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+315,@y+535,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+390,@y+535,60,60,Gosu::Color::WHITE, z=0, :default)
+
+      draw_rect(@x+90,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+165,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+240,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+315,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+390,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
+
+      draw_rect(@x+90,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+165,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+240,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+315,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
+      draw_rect(@x+390,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
+    elsif @stats.isInside?(x, y, xx, yy)
+      @font.draw("Dégâts: " + @players[0].damage.to_s,  @x-90, @y+620, 1, 1.0, 1.0, Gosu::Color::BLACK)
+      @font.draw("Déf. physique: " + @players[0].phy_def.to_s,  @x-90, @y+650, 1, 1.0, 1.0, Gosu::Color::BLACK)
+      @font.draw("Déf. énergie: " + @players[0].eng_def.to_s,  @x-90, @y+680, 1, 1.0, 1.0, Gosu::Color::BLACK)
+      @font.draw("Vitesse: " + @players[0].speed.to_s,  @x-90, @y+710, 1, 1.0, 1.0, Gosu::Color::BLACK)
+
+    @skills.isInside?(x, y, xx, yy)
+    @objects.isInside?(x, y, xx, yy)
   end
 
   def update(x,y)
@@ -31,7 +58,7 @@ class IHM < Gosu::Window
   end
 
   def click(x, y, xx, yy)
-    @carte.isInside?(x, y, xx, yy)  
+    @carte.isInside?(x, y, xx, yy)
     @stats.isInside?(x, y, xx, yy)
     @skills.isInside?(x, y, xx, yy)
     @objects.isInside?(x, y, xx, yy)
@@ -79,38 +106,7 @@ class IHM < Gosu::Window
 
   end
 
-  def tabContent(tab)#(tabName)
-    if tab==1 #(tabName == @tabs[0]) #carte
-      draw_rect(@x+465,@y+455,610,290,Gosu::Color::BLACK, z=0, :default) #carte
 
-      draw_rect(@x+90,@y+535,60,60,Gosu::Color::WHITE, z=0, :default) #skills
-      draw_rect(@x+165,@y+535,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+240,@y+535,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+315,@y+535,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+390,@y+535,60,60,Gosu::Color::WHITE, z=0, :default)
-
-      draw_rect(@x+90,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+165,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+240,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+315,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+390,@y+610,60,60,Gosu::Color::WHITE, z=0, :default)
-
-      draw_rect(@x+90,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+165,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+240,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+315,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
-      draw_rect(@x+390,@y+685,60,60,Gosu::Color::WHITE, z=0, :default)
-    elsif tab == 2#(tabName == @tabs[1]) #Stats
-      @font.draw("Dégâts: " + @players[0].damage.to_s,  @x-90, @y+620, 1, 1.0, 1.0, Gosu::Color::BLACK)
-      @font.draw("Déf. physique: " + @players[0].phy_def.to_s,  @x-90, @y+650, 1, 1.0, 1.0, Gosu::Color::BLACK)
-      @font.draw("Déf. énergie: " + @players[0].eng_def.to_s,  @x-90, @y+680, 1, 1.0, 1.0, Gosu::Color::BLACK)
-      @font.draw("Vitesse: " + @players[0].speed.to_s,  @x-90, @y+710, 1, 1.0, 1.0, Gosu::Color::BLACK)
-    #elsif #(tabName == @tabs[2]) #Capacités
-
-    #else #Objets
-
-    end
-  end
 
 end
 
@@ -124,3 +120,4 @@ Capacités (grisés pour ceux qui sont pas débloqués): - gauche: skills
 Objets:  - gauche: items portés actuellement par les personnages
         - droite: inventaire
 =end
+end
