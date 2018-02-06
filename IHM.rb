8be99f1cd3882fa027e4a93c31e77b9d2968796c
AoRailgun <leiwulong38@hotmail.fr>
2018-02-07 00:50:09 +0100

    tabs

--- IHM.rb
+++ IHM.rb
@@ -13,7 +13,6 @@ class IHM < Gosu::Window
     @carte = Button.new("Carte", @x-100,@y+350,130,50,Gosu::Color::WHITE, @font)
     @stats = Button.new("Stats", @x+35,@y+350,130,50,Gosu::Color::WHITE, @font)
     @skills = Button.new("Capacites", @x+170,@y+350,130,50,Gosu::Color::WHITE, @font)
-    @objects = Button.new("Objets", @x+305,@y+350,130,50,Gosu::Color::WHITE, @font)
   end
 
   def draw
@@ -26,7 +25,6 @@ class IHM < Gosu::Window
     @carte.update(x-100, y+350)
     @stats.update(x+35, y+350)
     @skills.update(x+170, y+350)
-    @objects.update(x+305, y+350)
   end
 
   def click(x, y, xx, yy)
@@ -37,7 +35,6 @@ class IHM < Gosu::Window
   end
 
   def box
-    draw_rect(@x+465,@y+355,610,290,Gosu::Color::BLACK, z=0, :default)
 =begin
     draw_rect(@x+100,@y+455,70,70,Gosu::Color::WHITE, z=0, :default)
     draw_rect(@x+190,@y+455,70,70,Gosu::Color::WHITE, z=0, :default)
@@ -63,7 +60,6 @@ class IHM < Gosu::Window
     @carte.draw
     @stats.draw
     @skills.draw
-    @objects.draw
 
 =begin
     @font.draw("Dégâts: " + @player.damage.to_s,  @x-90, @y+620, 1, 1.0, 1.0, Gosu::Color::BLACK)
@@ -72,6 +68,74 @@ class IHM < Gosu::Window
     @font.draw("Vitesse: " + @player.speed.to_s,  @x-90, @y+710, 1, 1.0, 1.0, Gosu::Color::BLACK)
     @font.draw("Liste d'objets",  @x+200, @y+540, 1, 1.0, 1.0, Gosu::Color::FUCHSIA)
 =end
+
+=begin onglet carte
+  draw_rect(@x+465,@y+455,610,290,Gosu::Color::BLACK, z=0, :default) #carte
+
+  draw_rect(@x+90,@y+430,60,60,Gosu::Color::WHITE, z=0, :default) #skills
+  draw_rect(@x+165,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+240,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+315,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+390,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+
+  draw_rect(@x+90,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+165,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+240,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+315,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+390,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+
+  draw_rect(@x+90,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+165,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+240,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+315,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+390,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+=end
+
+  @font.draw("Acvtives",  @x+258, @y+403, 1, 1.3, 1.3, Gosu::Color::BLACK)
+
+  draw_rect(@x+120,@y+430,60,60,Gosu::Color::WHITE, z=0, :default) #skills
+  draw_rect(@x+195,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+270,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+345,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+420,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+
+  draw_rect(@x+120,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+195,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+270,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+345,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+420,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+
+  draw_rect(@x+120,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+195,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+270,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+345,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+420,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+
+  @font.draw("Passives",  @x+750, @y+403, 1, 1.3, 1.3, Gosu::Color::BLACK)
+
+  draw_rect(@x+612,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+687,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+762,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+837,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+912,@y+430,60,60,Gosu::Color::WHITE, z=0, :default)
+
+  draw_rect(@x+612,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+687,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+762,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+837,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+912,@y+505,60,60,Gosu::Color::WHITE, z=0, :default)
+
+  draw_rect(@x+612,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+687,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+762,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+837,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+  draw_rect(@x+912,@y+580,60,60,Gosu::Color::WHITE, z=0, :default)
+
+
+
+
+
+
 
   end
 
@@ -85,6 +149,4 @@ Carte:  - gauche: interface combat avec skills
 stats:  stats des trois monsieurs placées coat à coat
 Capacités (grisés pour ceux qui sont pas débloqués): - gauche: skills
                                                   - droite: passifs
-Objets:  - gauche: items portés actuellement par les personnages
-        - droite: inventaire
 =end
