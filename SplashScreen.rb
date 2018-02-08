ced511170c69bd3a1af8215c3144b1f8e8d09dbc
Arttaaz <arttaaz@gmail.com>
2018-02-08 14:27:48 +0100

    smfljsfdefkj

--- SplashScreen.rb
+++ SplashScreen.rb
@@ -5,6 +5,10 @@ class SplashScreen
   def initialize(image, message)
     @image = image
     @message = message
+    @duree = 0
+  end
+
+  def show
     @duree = 180
   end
 
@@ -16,8 +20,8 @@ class SplashScreen
 
   def draw(x, y)
     if @duree > 0
-      Gosu.draw_rect()
-      @image.draw(x, y, 5)
+      Gosu.draw_rect(x, y, 500, 300, Gosu::Color::GRAY, 5)
+      @image.draw(x+200, y+100, 5)
     end
   end
 
